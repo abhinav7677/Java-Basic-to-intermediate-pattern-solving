37f15fafb756687a0aa31fd777032ae36eef0c66
Abhinav <125545001+abhinav7677@users.noreply.github.com>
2023-10-02 15:33:20 +0530

    Update q13Pattern1.java

--- q13Pattern1.java
+++ q13Pattern1.java
@@ -4,7 +4,7 @@ public class q13Pattern1 {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the number of star column you want ");
         int n=sc.nextInt();
-    for(int i=1;i<=n;i++){ //Loop
+    for(int i=1;i<=n;i++){ 
         for(int j=1;j<=i;j++){
             System.out.print("*\t");
         }
