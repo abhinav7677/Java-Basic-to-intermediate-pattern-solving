a10a623c58600dc650bc5f16b6ba999ed1b77f59
Abhinav <125545001+abhinav7677@users.noreply.github.com>
2023-10-03 19:25:36 +0530

    Update q31Pattern20.java

--- q31Pattern20.java
+++ q31Pattern20.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;public class q31Pattern20 { //
+import java.util.Scanner;public class q31Pattern20 { 
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
